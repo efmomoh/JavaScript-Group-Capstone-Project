ba0796061abf869d069c37a4e529755401a7359e
Obote <obotedenis256@gmail.com>
2023-09-13 16:47:32 +0300

    Fetch comments data

--- src/popup.js
+++ src/popup.js
@@ -2,5 +2,9 @@ const openCommentsPopup = async (showId) => {
   const commentsPopup = document.getElementById('comments-popup');
   const commentsList = document.getElementById('comments-list');
   commentsList.innerHTML = '';
-  
+
+  try {
+    const response = await fetch(`${TVMAZE_BASE_URL}/shows/${showId}/comments`);
+    const commentsData = await response.json(); 
+
 };
