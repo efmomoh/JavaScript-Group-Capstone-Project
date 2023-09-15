45f7defc0c6473c5a561c4691f5a24fe8e2aa3bc
efmomoh <efmomoh25@gmail.com>
2023-09-15 10:49:19 +0000

    Adds modules folder

---
+++ src/modules/homepage.js
@@ -0,0 +1,23 @@
+class Homepage {
+  seasonList = (arr) => {
+    const viewBox = document.querySelector('#view-box');
+    arr.forEach((element) => {
+      const div = document.createElement('div');
+      div.innerHTML = `
+      <div class="season-card">
+      <img src="${element.image.medium}" alt="season image" class="card-image">
+      <div class="season-details">
+        <h4 class="season-title">Season ${element.number}</h4>
+        <p class="likes">
+          <span class="likes-counter">0</span> likes <i class="fa-regular fa-heart"></i>
+        </p>
+      </div>
+      <a href="" class="comments btn-primary">Comment</a>
+    </div>
+    `;
+      viewBox.append(div);
+    });
+  };
+}
+
+export default Homepage;
