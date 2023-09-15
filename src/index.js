22c6c86ee396dc2d5b12e1f9462914e0eb69c6f4
efmomoh <efmomoh25@gmail.com>
2023-09-15 13:34:02 +0000

    Updates the index.html file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import RenderTVShowsItems from './modules/renderTVShowItems.js';
 import Homepage from './modules/homepage.js';
 
 const url = 'https://api.tvmaze.com/shows/6/seasons';
-const show = new RenderTVShowsItems(url);
-show.getData(url);
+const involvementBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const addLikesURL = `${involvementBaseURL}apps/ocBz5X2z8Jn2b71IzbDY`;
+
+const show = new RenderTVShowsItems(url, addLikesURL);
+show.getData(url, addLikesURL);
+
 const view = new Homepage();
 
 // On Page Load
