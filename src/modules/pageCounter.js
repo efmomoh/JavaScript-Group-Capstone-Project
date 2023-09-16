270e26c25fb4e9e58e733f632cc054623d4629d3
efmomoh <efmomoh25@gmail.com>
2023-09-16 20:23:20 +0000

    Modifies the pageCounter.js

--- src/modules/pageCounter.js
+++ src/modules/pageCounter.js
@@ -1,7 +1,7 @@
 const pageCount = () => {
   const seasons = document.querySelector('.seasons-count');
   const cards = document.querySelectorAll('.season-card').length;
-  seasons.innerHTML = ` Seasons ${cards}`;
+  seasons.innerHTML = ` Seasons (${cards})`;
 };
 
 export default pageCount;
