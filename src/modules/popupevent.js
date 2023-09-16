287090f9a0b4b7a9c40f55ee9fd6eebd2bda4fde
Obote <obotedenis256@gmail.com>
2023-09-16 19:28:35 +0300

    Update add comment

--- src/modules/popupevent.js
+++ src/modules/popupevent.js
@@ -1,18 +1,48 @@
+import addComment from './add.js';
+import { newComment, storedComments } from './api.js';
+import CommentPopup from './popup.js';
+
+const submitComment = async (e) => {
+  e.preventDefault();
+  const commList = document.querySelector('.comments-list');
+  const name = e.target.name.value;
+  const comm = e.target.comm.value;
+  const id = e.target.id.value;
+
+  await newComment({ id, name, comm });
+
+  const data = await storedComments(id);
+  const dataArr = data.data;
+  let html = '';
+  dataArr.forEach((item) => {
+    html += addComment(item.creation_date, item.username, item.comment);
+  });
+  commList.innerHTML = html;
+  e.target.reset();
+};
+
 const commentEvent = () => {
   const popup = document.querySelector('.comment-popup');
-  const popupContents = document.querySelectorAll('.popup-content');
-  const commentButtons = document.querySelectorAll('.btn-primary');
+  const commentButtons = document.querySelectorAll('.comment-btn');
 
   for (let index = 0; index < commentButtons.length; index += 1) {
-    commentButtons[index].addEventListener('click', (event) => {
-      event.preventDefault();
+    commentButtons[index].addEventListener('click', async () => {
+      const data = JSON.parse(localStorage.getItem('data'));
+      const btnId = commentButtons[index].id;
+      const element = data?.find((item) => Number(item.id) === Number(btnId));
       popup.classList.remove('d-none');
-      popupContents[index].classList.remove('d-none');
+      const popUpContents = new CommentPopup();
+      await popUpContents.seasonList(element);
 
-      const popupCloseButtons = popup.querySelectorAll('.popup-close');
-      popupCloseButtons[index].addEventListener('click', () => {
+      const forms = document.querySelector('.comment-form');
+
+      forms.addEventListener('submit', submitComment);
+
+      const popupCloseButton = popup.querySelector('.popup-close');
+      popupCloseButton.addEventListener('click', () => {
+        const popUpContents = document.querySelector('.popup-content');
         popup.classList.add('d-none');
-        popupContents[index].classList.add('d-none');
+        popUpContents.remove();
       });
     });
   }
